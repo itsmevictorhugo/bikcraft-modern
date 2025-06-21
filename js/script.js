e6a304298f3a3ca4577aeefc778dc2939e0924f9
Victor Hugo <csivictorhugo@gmail.com>
2025-06-21 11:59:08 -0300

    FAQ insurances page & a11y

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
 // Ativar Links do Menu
-const links = document.querySelectorAll('.header-menu a');
-
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLinks(link) {
-    const url = location.href;
-    const href = link.href;
-    if(url.includes(href)){
-        link.classList.add('ativo');
-    }
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
 links.forEach(ativarLinks);
-
 
 // Ativar Itens do Orçamento
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-    const elemento = document.getElementById(parametro);
-    if(elemento) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
     elemento.checked = true;
-    }
+  }
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("arial-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains('ativa');
+  console.log(ativa);
+  pergunta.setAttribute("arial-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
