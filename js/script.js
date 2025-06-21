4c15f8e83fb94d61ab940ce7ed4f21c100d5f37f
Victor Hugo <csivictorhugo@gmail.com>
2025-06-20 23:32:24 -0300

    URL parameters and adjustments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll('.header-menu a');
 
 
@@ -10,3 +11,16 @@ function ativarLinks(link) {
 }
 
 links.forEach(ativarLinks);
+
+
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if(elemento) {
+    elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
