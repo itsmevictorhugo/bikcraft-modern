838b5c37b58884781b2a24e65930caf48c9311bb
Victor Hugo <csivictorhugo@gmail.com>
2025-06-21 22:01:12 -0300

    bicycles gallery changing photo

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function ativarPergunta(event) {
   const resposta = document.getElementById(controls);
 
   resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains('ativa');
+  const ativa = resposta.classList.contains("ativa");
   console.log(ativa);
   pergunta.setAttribute("arial-expanded", ativa);
 }
@@ -42,3 +42,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicleta
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
